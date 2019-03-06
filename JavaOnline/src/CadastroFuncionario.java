3e370a59d6c6358a93838ebef4d0d2889df8efa6
barbara ellen <barbaraellen@outlook.com.br>
2019-03-06 08:54:58 -0300

    Curso de java online

---
+++ JavaOnline/src/CadastroFuncionario.java
@@ -0,0 +1,12 @@
+public class CadastroFuncionario {
+    public static void main (String[] args){
+        String nomeFuncionario = "José Bezerra";
+        double salarioFuncionario = 3000;
+        char sexoFuncionario = '\u004D';
+        int idadeFuncionario = 25;
+        String estadoCivilFuncionario = "solteiro";
+
+        System.out.println("O trabalhador "+nomeFuncionario+ " do sexo: "+sexoFuncionario+ ", idade: "+idadeFuncionario+", estado civil: "+estadoCivilFuncionario+" e salario: "+salarioFuncionario+", escontra-se empregado neste estabelecimento.");
+
+    }
+}
---
+++ JavaOnline/src/OperadoresLógicos.java
@@ -0,0 +1,8 @@
+public class OperadoresLógicos {
+    public static void main(String[] args){
+       // && = AND || = OR
+        int idade = 18;
+        float salario = 1000f;
+        System.out.println(idade>=18 || salario>=3000);
+    }
+}
