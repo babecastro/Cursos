421d38e7589f28d0e8783245388ebbafb60e0003
barbara ellen <barbaraellen@outlook.com.br>
2019-07-18 18:58:04 -0300

    reagrupando

--- JavaOnline/src/OperadoresLógicos.java
+++
@@ -1,8 +0,0 @@
-public class OperadoresLógicos {
-    public static void main(String[] args){
-       // && = AND || = OR
-        int idade = 18;
-        float salario = 1000f;
-        System.out.println(idade>=18 || salario>=3000);
-    }
-}
